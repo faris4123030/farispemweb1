53ddb3d2e5c08682b18c3548cc59025598c8ceb5
raihan salman alfarisi <raihansalmanalfarisi99@gmail.com>
2024-12-12 11:45:17 +0700

    pertemuan 10

---
+++ pertemuan 10/tambah_aksi.php
@@ -0,0 +1,16 @@
+<?php 
+// koneksi database
+include 'koneksi.php';
+ 
+// menangkap data yang di kirim dari form
+$nama = $_POST['nama'];
+$nis = $_POST['nis'];
+$alamat = $_POST['alamat'];
+ 
+// menginput data ke database
+mysqli_query($koneksi,"INSERT into siswaa values('','$nama','$nis','$alamat')");
+ 
+// mengalihkan halaman kembali ke index.php
+header("location:index.php");
+ 
+?>
